a9720485fcaedab28d7ac4f9401d7822457f21de
milad seyfi <miladseify5@gmail.com>
2023-05-18 14:29:51 +0330

    add emailforceAndSuperUSer

--- app/core/models.py
+++ app/core/models.py
@@ -15,8 +15,19 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_Field):
         """create , save and return new user"""
+        if not email:
+            raise ValueError("User must have an email address")
         user = self.model(email=self.normalize_email(email), **extra_Field)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """create and return new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -30,3 +30,17 @@ class ModelTests(TestCase):
             user = get_user_model()\
                 .objects.create_user(email, 'sample123')  # type: ignore
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """test that creating a user without an email raises a ValueError"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'pass123')  # type: ignore
+
+    def test_create_superuser(self):
+        """test that creating a superuser"""
+        user = get_user_model().objects.create_superuser(  # type: ignore
+            'test@test.com',
+            'pass123'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
